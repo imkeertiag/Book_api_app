aebcfecdddc7e28a44d29bba1cc861c590c8d365
Keerti <keertiagarwal2003@gmail.com>
2024-06-30 14:52:28 +0530

    new file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Components/style.css'
+import Main from './Components/Main';
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
